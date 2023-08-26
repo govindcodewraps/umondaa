fc1e4adc70340733bb7d9abdf509558e5b6d88e0
Govind Rajpoot <govindcodewraps@gmail.com>
2023-08-26 17:22:48 +0530

    dreawer

--- lib/screens/drawermenu/Aboutus.dart
+++ lib/screens/drawermenu/Aboutus.dart
@@ -16,76 +16,96 @@ class Aboutus extends StatelessWidget {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Color.fromARGB(255, 2, 70, 40),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('About us',
-          style: TextStyle(color:  Color.fromARGB(255, 255, 255, 255)),),
-      ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-          height: 700,
+      appBar: buildAppBar(context),
+      body: ListView(
+              children: [
+                Container(
+                  child: Image.asset('assets/appbarlogo.png',
+                    height: 70,
+                    width: 100,),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text("At UMONDA, we create pathways to connect sellers and buyers within the UAE. We are offering a platform to our users, which strives to simplify the selling and buying process of pre-owned goods and items with taking care of pickup and delivery, even dismantling and assembly, if needed.",
+                      style: TextStyle(fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text("Our aim is to assist our users in a better and more convenient way, through following features:",
+                      style: TextStyle(fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text("1. Buy and Sell with just one click: No bargaining phone calls, no personal house visits etc.",
+                      style: TextStyle(fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text("2. Sell at fixed price: Buyer accepts price of seller, and can buy instantly ('buy now').",
+                      style: TextStyle(fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text("3. Make an offer: Give the buyer the option to offer a certain purchase price, and sell within the seller's set minimum selling price.",
+                      style: TextStyle(fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text("4. Free Delivery: Delivery or shipping cost are included in the purchase price and commission paid by buyer and seller on each transaction.",
+                      style: TextStyle(fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0))),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 60),
+                  child: Text.rich(TextSpan(
+                      children: [
+                        TextSpan(text: '5. Dismantling and Assembly: When selling and buying furniture for instance, for an optional extra charge of AED 100,', style: TextStyle(fontSize: 20)),
+                        TextSpan(text: 'UMONDA LLC will take care of dismantling and assembling the item at each respective location.', style: TextStyle(fontSize: 20)),
+                      ]
+                  ),
+                  ),
+                ),
+              ],
+            ),
+    );
 
-          child: Column(
+  }
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.black),
+      backgroundColor: Color.fromARGB(240, 243, 237, 237),
+      title: buildAppBarTitleOption(context),
+      elevation: 0.0,
+      titleSpacing: 0,
 
-            children: [
-              Container(
-                child: Image.asset('assets/topbarlogo1.png',
-                  height: 20,
-                  width: 50,),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("At UMONDA, we create pathways to connect sellers and buyers within the UAE. We are offering a platform to our users, which strives to simplify the selling and buying process of pre-owned goods and items with taking care of pickup and delivery, even dismantling and assembly, if needed.",
-                    style: TextStyle(fontSize: 15,
-                        color: Color.fromARGB(255, 0, 0, 0))),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("Our aim is to assist our users in a better and more convenient way, through following features:",
-                    style: TextStyle(fontSize: 20,
-                        color: Color.fromARGB(255, 0, 40, 0))),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("1. Buy and Sell with just one click: No bargaining phone calls, no personal house visits etc.",
-                    style: TextStyle(fontSize: 15,
-                        color: Color.fromARGB(255, 0, 0, 0))),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("2. Sell at fixed price: Buyer accepts price of seller, and can buy instantly ('buy now').",
-                    style: TextStyle(fontSize: 15,
-                        color: Color.fromARGB(255, 0, 0, 0))),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("3. Make an offer: Give the buyer the option to offer a certain purchase price, and sell within the seller's set minimum selling price.",
-                    style: TextStyle(fontSize: 15,
-                        color: Color.fromARGB(255, 0, 0, 0))),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("4. Free Delivery: Delivery or shipping cost are included in the purchase price and commission paid by buyer and seller on each transaction.",
-                    style: TextStyle(fontSize: 15,
-                        color: Color.fromARGB(255, 0, 0, 0))),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Text("5. Dismantling and Assembly: When selling and buying furniture for instance, for an optional extra charge of AED 100, UMONDA LLC will take care of dismantling and assembling the item at each respective location. ",
-                    style: TextStyle(fontSize: 15,
-                        color: Color.fromARGB(255, 0, 0, 0))),
-              ),
-            ],
+    );
+  }
+
+  Widget buildAppBarTitleOption(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 0),
+      child: Row(
+        children: [
+          Container(
+            width: 20,
+            // child: UsefulElements.backButton(context, color: "white"),
           ),
+          Container(
+            padding: EdgeInsets.only(left: 0),
 
-        ),
+            child: Text('About us',
+              style: TextStyle(color:  Colors.black,),
+            ),
+          )
+
+        ],
       ),
     );
   }
+
 }
--- lib/screens/drawermenu/shipping_delivery.dart
+++ lib/screens/drawermenu/shipping_delivery.dart
@@ -16,16 +16,7 @@ class ShippingDelivery extends StatelessWidget {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Color.fromARGB(255, 2, 70, 40),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Shipping & Delivery',
-          style: TextStyle(color:  Color.fromARGB(255, 255, 255, 255)),),
-      ),
+      appBar: buildAppBar(context),
       body: ListView(
         // child: Container(
         //   margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
@@ -35,14 +26,15 @@ class ShippingDelivery extends StatelessWidget {
 
         children: [
           Container(
-            child: Image.asset('assets/topbarlogo1.png',
-              height: 20,
-              width: 50,),
+            padding: EdgeInsets.only(top: 15.0, bottom: 5),
+            child: Image.asset('assets/appbarlogo.png',
+              height: 70,
+              width: 100,),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("What are the delivery charges?",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
@@ -54,108 +46,149 @@ class ShippingDelivery extends StatelessWidget {
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Why does the delivery date not correspond to the delivery timeline of X-Y business days?",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("It is possible that the Seller or our courier partners have a holiday on the day you placed your order and the date of delivery. In this case, we add a day to the estimated date. Some courier partners and Sellers do not work or are not available on certain days.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("What is the estimated delivery time?",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Sellers generally procure and ship the items as soon as possible. Business days exclude public holidays and Sundays.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Estimated delivery time depends on the following factors:",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("The Seller offers the product.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Product's availability with the Seller.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("The destination to which you want the order to be shipped to, and the location of the Seller.",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("There are no hidden delivery charges.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Why does the estimated delivery time vary for each seller?",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("You have probably noticed varying estimated delivery times for sellers of the product you are interested in. Delivery times are influenced by seller's availability, geographic location of the Seller, your shipping destination and the courier partner's time-to-deliver to your location.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("I need to return an item, how do I arrange for a pick-up?",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Returns are excluded for used and pre-owned items. ",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("For returns on new products, contact us to initiate a return. You will receive a call explaining the process, once you have initiated a return.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("Whenever possible our courier partner will facilitate the pick-up of the item. In case, the pick-up cannot be arranged, you can return the item through a third-party courier service. Return fees are paid by the Seller.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
             margin: EdgeInsets.all(10.0),
             child: Text("I did not receive my order but got a delivery confirmation SMS/Email.",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
           Container(
-            margin: EdgeInsets.all(10.0),
+            margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 60),
             child: Text("In case the product was not delivered and you have received a delivery confirmation email/SMS, kindly report this issue to us within 7 days from the date of delivery confirmation.",
-                style: TextStyle(fontSize: 15,
+                style: TextStyle(fontSize: 20,
                     color: Color.fromARGB(255, 0, 0, 0))),
           ),
+
         ],
       ),
 
     );
   }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.black),
+      backgroundColor: Color.fromARGB(240, 243, 237, 237),
+      title: buildAppBarTitleOption(context),
+      elevation: 0.0,
+      titleSpacing: 0,
+
+    );
+  }
+
+  // Widget buildAppBarTitle(BuildContext context) {
+  //   return AnimatedCrossFade(
+  //       firstChild: buildAppBarTitleOption(context)
+  //   );
+  // }
+
+  Widget buildAppBarTitleOption(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 0),
+      child: Row(
+        children: [
+          Container(
+            width: 20,
+            // child: UsefulElements.backButton(context, color: "white"),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 0),
+
+            child: Text('Shipping & Delivery',
+              style: TextStyle(color:  Colors.black,),
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+
 }
